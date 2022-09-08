2e6f5520d306df34d6e51f9d2888b1b5f9229c44
Fakhri <43924526+baron07@users.noreply.github.com>
2022-09-08 13:08:55 +0700

    Menambahkan file BIN to BCD

---
+++ Praktikum 5/Binary to BCD/kodeP5B.vhd
@@ -0,0 +1,28 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.std_logic_unsigned.all;
+
+entity kodeP5B is
+	port( b : in std_logic_vector(7 downto 0);
+			p: out STD_LOGIC_VECTOR(9 downto 0));
+end kodeP5B;
+
+architecture Behavioral of kodeP5B is
+begin
+	bcd1:process(b)
+	variable z :STD_LOGIC_VECTOR(17 downto 0);	begin
+		for i in 0 to 17 loop
+				z(i):= '0';
+		end loop;
+		z(10 downto 3):=b;
+			for i in 0 to 4 loop
+				if z(11 downto 8)> 4 then
+						z(11 downto 8):= z(11 downto 8)+3;
+		end if;
+		if z(15 downto 12)>4 then
+				z(15 downto 12):= z(15 downto 12)+3;
+		end if;
+		z(17 downto 1):= z(16 downto 0);
+		end loop;
+		p <= z(17 downto 8);	end process bcd1;
+end Behavioral;
