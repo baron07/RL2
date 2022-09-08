834c55ebe979b31e72b2062a3b27eb7c012b5672
Fakhri <43924526+baron07@users.noreply.github.com>
2022-09-08 13:16:25 +0700

    Menambahkan file GTB

---
+++ Praktikum 5/Gray to Binary/kodeP5D.vhd
@@ -0,0 +1,20 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity kodeP5D is
+	port(g:in STD_LOGIC_VECTOR(3 downto 0);
+			b:out STD_LOGIC_VECTOR(3 downto 0));
+end kodeP5D;
+
+architecture Behavioral of kodeP5D is
+begin
+	process(g)
+		variable bv :STD_LOGIC_VECTOR(3 downto 0);
+	begin
+		bv(3):= g(3);
+		for i in 2 downto 0 loop
+			bv(i):= bv(i+1)xor g(i);
+		end loop;
+		b <= bv;
+	end process;
+end Behavioral;
